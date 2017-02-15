961e186bf6be8ec4844a0794a725c2dfbc30b9b3
Unknown <aa5270@nyu.edu>
2017-02-14 19:08:59 -0500

    Added new

---
+++ CodeLab_HW3/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		moveSpeed = 1f;
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (moveSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis ("Vertical") * Time.deltaTime);
+		if(Input.GetButtonDown("Jump")){
+			rb.velocity = new Vector3 (0, 8, 0);
+		}
+
+	}
+}
